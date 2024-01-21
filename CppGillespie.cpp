5c801f632395774465444e51720d5eb4d61c6f4c
RuiboZhang98 <ruibo.zhang98@gmail.com>
2024-01-20 17:08:27 -0800

    include command line arguments in the main simulation

--- CppGillespie.cpp
+++ CppGillespie.cpp
@@ -2,10 +2,11 @@
 #include <fstream>
 #include <vector>
 #include <random>
+#include <string>
 
-#include <Eigen/Dense>
+#include <Eigen/Dense> // path: G:\\code\\cfiles\\eigen-3.4.0
 
-using std::vector;
+using std::vector, std::string;
 using Eigen::Ref, Eigen::ArrayXXd, Eigen::ArrayXd, Eigen::ArrayXi;
 
 // function prototypes
@@ -15,10 +16,53 @@ const Eigen::Ref<const Eigen::ArrayXXd>& matrix, const int& ntype);
 double lifespan(const Eigen::Ref<const Eigen::ArrayXXd>& weights, std::mt19937_64& rng);
 int increment_type(const Eigen::Ref<const Eigen::ArrayXXd>& weights, std::mt19937_64& rng, int &flag);
 
-int main()
+int main(int argc, char** argv)
 {
     using std::cout, std::endl;
     using Eigen::Map, Eigen::RowVectorXd, Eigen::VectorXd, Eigen::seq; //, Eigen::last;
+
+    // parameters that are related to commandline arguments
+    string outputfile = ""; // related to the output file name
+    long long unsigned int seed = 0;    // random seed
+    int runs = 10;        // number of realizations
+    // read parameters
+    for (int i = 1; i < argc; ++i) {
+        // Errors
+        if ((string)argv[i] == "-o") {
+            if (i == argc-1) {
+                cout << "Error: no output file" << endl;
+                return 1;
+            } else {
+                // set outputfile
+                outputfile = (string)argv[i+1];
+            }
+        }
+
+        if ((string)argv[i] == "--seed") {
+            if (i == argc-1) {
+                cout << "Error: please enter the seed for generating random numbers" << endl;
+                return 1;
+            } else {
+                // set number of threads
+                seed = (long long unsigned int)atoi(argv[i+1]);
+            }
+        }
+
+        if ((string)argv[i] == "--runs") {
+            if (i == argc-1) {
+                cout << "Error: please enter the number of runs" << endl;
+                return 1;
+            } else {
+                // set number of threads
+                runs = atoi(argv[i+1]);
+            }
+        }
+
+        if (outputfile == "") {
+            cout << "Error: no output file" << endl;
+            return 1;
+        }
+    }
 
     // branching process parameters
     const ArrayXd initial_population { {1000, 0, 0, 0 } }; 
@@ -43,17 +87,20 @@ int main()
     cout << "transition rate matrix = \n" << transition_rates << endl;
 
     // Simulation Parameters
-    const double tmax = 15;          // ending time of simulations
-    // have to add a little bit time to actually reach tmax
-    const int runs = 1;        // number of realizations
+    const double tmax = 20;          // ending time of simulations
     const double tgrid =  1;    // time grid length. On grid points populations are recorded
     const int datalen = (int)(tmax / tgrid);  // length of recorded data
     int flag;
 
+    // output file name
+    string outputfile_population_filename = outputfile + "_population_seed" 
+    + std::to_string((int)seed) + "_runs" + std::to_string(runs) + ".txt";
+    string outputfile_waitingtime_filename = outputfile + "_waitingtime_seed" 
+    + std::to_string((int)seed) + "_runs" + std::to_string(runs) + ".txt";
+
     cout << "number of runs = " << runs << endl; // print some important parameters
     cout << "datalen = " << datalen << endl;
 
-    long long unsigned int seed = 0;    // random seed
     std::mt19937_64 mt { seed };        // random number generator. When having concurrency, the seed for each thread should be different 
     
     const VectorXd record_time = VectorXd::LinSpaced(datalen, 0, tmax);
@@ -61,15 +108,14 @@ int main()
     ArrayXXd weights(ntype, ntype);
     // In the following population data array, each colume stores a single realization.
     // The first column is used to store the time grid;
-    ArrayXXd population_data = ArrayXXd::Constant(datalen * ntype, runs + 1,-1.0); 
-    for (int i = 0; i < ntype; ++i){
-        population_data.block(i * datalen, 0, datalen, 1) = record_time;
-    }
+    ArrayXXd population_data = ArrayXXd::Constant(datalen * runs, ntype + 1,-1.0);
+    ArrayXXd waitingtime_data = ArrayXXd::Constant(datalen, ntype + 1, 0.0);  
+
+    population_data.block(0, 0, datalen, 1) = record_time; // write times in the first column
+    waitingtime_data.block(0, 0, datalen, 1) = record_time;
+
     int data_index, run_index, change_index;
     double t;
-    // save to a file
-    std::ofstream outFile;
-    outFile.open("data.txt");
 
     // The main body of the Gillespie Simulation
     for (run_index = 0; run_index < runs; ++run_index){
@@ -78,10 +124,10 @@ int main()
         data_index = 0;     // reset the index of data
         while (t < tmax){
             for ( ; record_time(data_index) <= t ; ++data_index){
-                for (int i = 0; i < ntype; ++i)
-                    population_data(i * ntype + data_index, run_index + 1) = population(i);
-                cout << "record_time = " << record_time(data_index)
-                << " Population = " << population.transpose() << endl;
+                population_data.block(run_index * datalen + data_index, 1, 1, 4) = population.transpose();
+                waitingtime_data.block(data_index, 1, 1, 4) += (population.transpose() > 0).cast<double>();
+                // cout << "record_time = " << record_time(data_index)
+                // << " Population = " << population.transpose() << endl;
             }
             weights = transition_rates.colwise() * Map<RowVectorXd>(population.data(), population.size()).array().transpose();
             change_index = increment_type(weights, mt, flag);
@@ -92,18 +138,24 @@ int main()
             // cout << "t = " << t << endl;
         }
         // for the end time
-        cout << "record_time = " << record_time(data_index)
-            << " Population = " << population.transpose() << endl;
-        for (int i = 0; i < ntype; ++i)
-            population_data(i * ntype + data_index, run_index + 1) = population(i);
+        // cout << "record_time = " << record_time(data_index)
+           // << " Population = " << population.transpose() << endl;
+        population_data.block(run_index * datalen + data_index, 1, 1, 4) = population.transpose();
+        waitingtime_data.block(data_index, 1, 1, 4) += (population.transpose() > 0).cast<double>();
         cout << "The " << run_index + 1 << "th run finishes" << endl;
     }
+    // normalize waiting time data
+    waitingtime_data /= runs;
 
-    outFile << endl << population_data << endl;
-    outFile.close();
+    // save to a file
+    std::ofstream outFile_population, outFile_waitingtimes;
+    outFile_population.open(outputfile_population_filename);
+    outFile_population << endl << population_data << endl;
+    outFile_population.close();
 
-    cout << "Finished. Press any key to exit.";
-    getchar();
+    outFile_population.open(outputfile_waitingtime_filename);
+    outFile_population << endl << waitingtime_data << endl;
+    outFile_population.close();
 
     return 0;
 }
